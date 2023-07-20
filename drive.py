94166cca1b87d1d588a47b249e39f1a421060bf9
Yuzou0317 <578935294@qq.com>
2023-07-20 16:13:00 +0200

    update the drive.py

--- drive.py
+++ drive.py
@@ -1,19 +1,22 @@
-# This server used to receive images and current vehicle data from the client, passes the images to a trained classifier to predict new vehicle commands, 
+# This server used to receive images and current vehicle data from the client, passes the images to a trained classifier to predict new vehicle commands,
 # and then sends these commands back to Unity to control the car for autonomous driving.
 
-# now we only test the communication between the client and server at first, 
+# now we only test the communication between the client and server at first,
 # so we just give the fixed command to see wether the car will be controlled and wether the images succesffly recieved
 # lately we can also add code to realize autonomous driving
 
 import socketio
+
 # concurrent networking
 import eventlet
+
 # web server gateway interface
 import eventlet.wsgi
 from flask import Flask
 import base64
 import cv2
 import numpy as np
+
 # from io import BytesIO
 import time
 import os
@@ -30,16 +33,13 @@ fps = 0
 
 @sio.on("send_image")
 def on_image(sid, data):
-    #make the variables global to calculate the fps 
+    # make the variables global to calculate the fps
     global frame_count, frame_count_save, prev_time, fps
-    #print("image recieved!")
+    # print("image recieved!")
     img_data = data["image"]
     img_bytes = base64.b64decode(img_data)
     # Decode image from base64 format，将字典里抽取出来的字符串转换为字节串类型
     img = cv2.imdecode(np.frombuffer(img_bytes, dtype=np.uint8), cv2.IMREAD_COLOR)
-
-    # # create a variable to identify every frame of image for the lately image-save
-    # frame_count_save += 1
 
     # Calculate and print fps
     frame_count += 1
@@ -49,20 +49,6 @@ def on_image(sid, data):
         print(f"FPS: {fps:.2f}")
         prev_time = time.time()
         frame_count = 0
-
-    # # split the merged image
-    # _, width, _ = img.shape
-    # single_width = width // 3
-
-    # img1 = img[:, :single_width, :]
-    # img2 = img[:, single_width : 2 * single_width, :]
-    # img3 = img[:, 2 * single_width :, :]
-
-    # # save the images
-    # save_folder = os.path.expanduser("~/Desktop/receivedImage")
-    # os.makedirs(save_folder, exist_ok=True)
-    # filename = os.path.join(save_folder, f"image{frame_count_save:02d}.jpg")
-    # cv2.imwrite(filename, img)
 
     # show the recieved images on the screen
     if img is not None and img.shape[0] > 0 and img.shape[1] > 0:
@@ -75,21 +61,25 @@ def on_image(sid, data):
     else:
         print("Invalid image data")
 
+
 # listen for the event "vehicle_data"
 @sio.on("vehicle_data")
 def vehicle_command(sid, data):
-    # print("data recieved!")
+    print("data recieved!")
     steering_angle = float(data["steering_angle"])
     throttle = float(data["throttle"])
+    brake = float(data["brake"])
+    velocity = float(data["velocity"])
+
+    print(f"velocity of the car: {velocity}")
 
     if data:
-        steering_angle = 0
+        steering_angle = 0.7
         throttle = 0.3
+        brake = 0
 
-        send_control(steering_angle, throttle)
+        send_control(steering_angle, throttle, brake)
     else:
-        # send the data to unityClient
-        # sio.emit("manual", data={})
         print("data is empty")
 
 
@@ -97,18 +87,21 @@ def vehicle_command(sid, data):
 def connect(sid, environ):
     # sid for identifying the client connected表示客户端唯一标识符，environ表示其连接的相关环境信息
     print("Client connected")
-    send_control(0, 0)
+    send_control(0, 0, 0)
+
 
 # Define a data sending function to send processed data back to unity client
-def send_control(steering_angle, throttle):
+def send_control(steering_angle, throttle, brake):
     sio.emit(
         "control_command",
         data={
             "steering_angle": steering_angle.__str__(),
             "throttle": throttle.__str__(),
+            "brake": brake.__str__(),
         },
         skip_sid=True,
     )
+
 
 @sio.event
 def disconnect(sid):
